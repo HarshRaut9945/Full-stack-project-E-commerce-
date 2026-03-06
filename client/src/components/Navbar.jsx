a8fa218633e7398c5a04aa5ce28f515771b9811f
HarshRaut9945 <harshraut9945@gmail.com>
2026-03-06 19:45:50 +0530

    add

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,16 +7,44 @@ const Navbar = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
 
-  const {Setfilterdata,products}=useContext(AppContext)
+  const { setFilterData, products } = useContext(AppContext);
+
+  const filterbycategory = (cat) => {
+    if (cat === products) {
+      setFilterData(products);
+    } else {
+      setFilterData(
+        products.filter((data) =>
+          data.category.toLowerCase() === cat.toLowerCase()
+        )
+      );
+    }
+  };
+
+const filterbycategory = (cat) => {
+    if (cat === products) {
+      setFilterData(products);
+    } else {
+      setFilterData(
+        products.filter((data) =>
+          data.category.toLowerCase() === cat.toLowerCase()
+        )
+      );
+    }
+  };
+
+
+
   const sumbitHandler = (e) => {
     e.preventDefault();
     navigate(`/product/search/${searchTerm}`);
-    searchTerm(" ");
+    setSearchTerm("");
   };
+
   return (
     <>
       <div className="nav sticky-top">
-        <div className="nav_bar  ">
+        <div className="nav_bar">
           <Link
             to={"/"}
             className="left"
@@ -24,6 +52,7 @@ const Navbar = () => {
           >
             <h3>MERN E-commerce</h3>
           </Link>
+
           <form className="search_bar" onSubmit={sumbitHandler}>
             <FaSearch />
             <input
@@ -33,16 +62,49 @@ const Navbar = () => {
               placeholder="Search Product"
             />
           </form>
+
           <div className="right">
-            <button className="btn btn-warning mx-3   ">cart</button>
-            <button className="btn btn-warning mx-3   ">profile</button>
-            <Link to={"/login"} className="btn btn-secondary  mx-3   ">login</Link>
-            <Link to={'/register'} className="btn btn-info mx-3   ">register</Link>
-            <button className="btn btn-warning mx-3   ">logout</button>
+            <button className="btn btn-warning mx-3">cart</button>
+            <button className="btn btn-warning mx-3">profile</button>
+
+            <Link to={"/login"} className="btn btn-secondary mx-3">
+              login
+            </Link>
+
+            <Link to={"/register"} className="btn btn-info mx-3">
+              register
+            </Link>
+
+            <button className="btn btn-warning mx-3">logout</button>
           </div>
         </div>
+
         <div className="sub_bar">
-          
+          <div className="items" onClick={() => filterbycategory(products)}>
+            No Filter
+          </div>
+
+          <div className="items" onClick={() => filterbycategory("mobiles")}>
+            Mobile
+          </div>
+
+          <div className="items" onClick={() => filterbycategory("laptops")}>
+            Laptops
+          </div>
+
+          <div className="items" onClick={() => filterbycategory("cameras")}>
+            Cameras
+          </div>
+
+          <div className="items" onClick={() => filterbycategory("headphone")}>
+            Headphones
+          </div>
+
+          <div className="items">15999</div>
+          <div className="items">25999</div>
+          <div className="items">49999</div>
+          <div className="items">6999</div>
+          <div className="items">89999</div>
         </div>
       </div>
     </>
